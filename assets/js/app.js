346b1ec484fa061874aa3e88bad3350de9a2df39
sbeinart <sbeinart@iupui.edu>
2013-03-04 19:58:18 -0500

    got jqtouch in the assests, added an img for blogs updated some blogs

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+;(function($, window) {
+	
+    $(function(){
+	
+	    var jQT = new $.jQTouch({
+	        statusBar: 'black-translucent',
+	        preloadImages: []
+	    });
+
+    });
+    
+	$('.toggle').click(function(e) {
+		var $t = $(this);
+		var id = $t.attr('href');
+		var target = $(id);
+		
+		console.log(id);
+		
+		if(target.css('display') != 'none') {
+			target.removeClass('show');
+		}
+		else {
+			target.addClass('show');
+		}
+		
+		e.preventDefault();
+	});
+	
+	$('.bars').click(function(e) {
+		var $t = $(this);
+		
+		if($t.hasClass('rotate')) {
+			$t.removeClass('rotate');
+		}
+		else {
+			$t.addClass('rotate');
+		}
+				
+		e.preventDefault();
+	});
+	
+}(jQuery, this));
